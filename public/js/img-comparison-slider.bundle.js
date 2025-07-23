b32bb99eb7d523a18d69c663dc8b102b23e52175
gabriel20xx <gabrielfranz31@gmail.com>
2025-07-24 00:26:59 +0200

    fix

---
+++ public/js/img-comparison-slider.bundle.js
@@ -0,0 +1,1 @@
+import './img-comparison-slider/index.js';
--- public/js/img-comparison-slider.js
+++ public/js/img-comparison-slider.js
@@ -1,2 +1,2 @@
 // Expose img-comparison-slider as a web component for browser use
-import '/node_modules/img-comparison-slider/dist/index.js';
+import '/js/img-comparison-slider/index.js';
